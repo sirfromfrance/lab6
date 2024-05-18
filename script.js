0088e5992778a3afa333f6233cc592b725c9bf85
sirfromfrance <gritsenko5555@gmail.com>
2024-05-18 12:30:06 +0300

    fixed some errors

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function handleLoginFormSubmit(loginForm) {
   const passwordValue = passwordInput.value.trim();
 
   if (!emailValue || !passwordValue) {
-    alert("All form fields must be filled in");
+    alert("Будь ласка, заповність всі поля");
     return false;
   }
 
@@ -64,42 +64,32 @@ function handleLoginFormSubmit(loginForm) {
   return true;
 }
 //10
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+function colorFunc() {
+  return `#${Math.trunc(Math.random() * 16777215).toString(16)}`;
 }
 
-function createBoxes(amount) {
-  let markup = "";
-  let startWidth = 30;
-  let starHeight = 30;
-  for (let i = 0; i < amount; i++) {
-    markup += `<div style="background-color: ${getRandomHexColor()}; width: ${startWidth}px; height: ${starHeight}px"></div>`;
+function box(quantity) {
+  let content = "";
+  let startWidth = 10;
+  let starHeight = 10;
+  for (let i = 0; i < quantity; i++) {
+    content += `<div style="background-color: ${colorFunc()}; width: ${startWidth}px; height: ${starHeight}px"></div>`;
     startWidth += 10;
     starHeight += 10;
   }
-  boxStore.insertAdjacentHTML("beforeend", markup);
+  boxStore.insertAdjacentHTML("beforeend", content);
 }
 
-function destroyBoxes() {
+function deleteBox() {
   boxStore.innerHTML = "";
-}
-
-function isInputValid() {
-  if (input.value > inputMax || input.value < inputMin) {
-    alert(`Write in range from ${inputMin} to ${inputMax}`);
-    input.value = 1;
-  }
 }
 
 const boxStore = document.querySelector("#boxes");
 const createButton = document.querySelector("[data-create]");
 const destroyButton = document.querySelector("[data-destroy]");
 const input = document.querySelector("#controls input");
-const inputMin = Number(input.getAttribute("min"));
-const inputMax = Number(input.getAttribute("max"));
 
 input.value = 1;
 
-createButton.addEventListener("click", () => createBoxes(input.value));
-destroyButton.addEventListener("click", () => destroyBoxes());
-input.addEventListener("input", () => isInputValid());
+createButton.addEventListener("click", () => box(input.value));
+destroyButton.addEventListener("click", () => deleteBox());
